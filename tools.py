22083d567be7b551eced42e514f8880c5a16dbca
nsj2121 <nsjnsj1009@inha.edu>
2026-07-05 00:54:26 +0900

    Update tools.py

--- tools.py
+++ tools.py
@@ -9,3 +9,15 @@ def add_grade(student_id,subject,score):
 
 def add_attendance(student_id,status):
     students[student_id]['출결'].append(status)
+    
+def get_selected_students(target_names):
+    selected_data = []
+    
+    for student_id, info in students.items():
+        if info['이름'] in target_names:
+            python_score = info['성적'].get('파이썬', 0)
+            attendance = " ".join(info['출결'])
+            
+            selected_data.append([student_id, info['이름'], python_score, attendance])
+            
+    return selected_data
